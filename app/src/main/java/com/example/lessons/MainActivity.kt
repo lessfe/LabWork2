60b8d81fde2e5bc269963278ba69703c0511af26
lessfe <vladislav_fedotov_00@mail.ru>
2023-09-20 14:07:38 +0300

    lastest update

--- app/src/main/java/com/example/lessons/MainActivity.kt
+++ app/src/main/java/com/example/lessons/MainActivity.kt
@@ -1,44 +1,32 @@
 package com.example.lessons
 
-import android.icu.text.ListFormatter.Width
+import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.lessons.ui.theme.LessonsTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
 
@@ -46,19 +34,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var text_1 by rememberSaveable {
-                mutableStateOf("0.0")
-            }
             var text by rememberSaveable {
                 mutableStateOf("0.0")
             }
+
+            var text_1 by rememberSaveable {
+                mutableStateOf("0.0")
+            }
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.Start,
             ) {
                 Button(onClick = {
-                    val sumNumPl = text.toString().toFloat() + text_1.toString().toFloat()
+                    val sumNumPl = text.toFloat() + text_1.toFloat()
                     val str_sum = sumNumPl.toString()
                     text = str_sum
                 }) {
@@ -86,7 +76,7 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.End,
             ) {
                 Button(onClick = {
-                    val sumNumPl = text.toString().toFloat() - text_1.toString().toFloat()
+                    val sumNumPl = text.toFloat() - text_1.toFloat()
                     val str_sum = sumNumPl.toString()
                     text = str_sum
                 }) {
@@ -122,28 +112,32 @@ class MainActivity : ComponentActivity() {
                     label = { Text("") }
                 )
             }
+
+            Column(
+                modifier = Modifier.offset(260.dp, 280.dp),
+
+                ) {
+                Button(onClick = {
+                    intent.putExtra("TextField", text)
+                    startActivity(intent)
+                }) {
+                    Text(text = "Перейти")
+                }
+
+            }
         }
     }
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun Greeting(name: String, modifier: Modifier = Modifier) {
-        var text_1 by rememberSaveable {
-            mutableStateOf("0.0")
-        }
-        var text by rememberSaveable {
-            mutableStateOf("0.0")
-        }
+
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.Start,
         ) {
-            Button(onClick = {
-                val sumNumPl = text.toString().toFloat() + text_1.toString().toFloat()
-                val str_sum = sumNumPl.toString()
-                text = str_sum
-            }) {
+            Button(onClick = {}) {
                 Text(text = "Добавить")
             }
         }
@@ -154,10 +148,8 @@ class MainActivity : ComponentActivity() {
         ) {
             TextField(
                 modifier = Modifier.width(70.dp),
-                value = text_1,
-                onValueChange = {
-                    text_1 = it
-                },
+                value = "",
+                onValueChange = {},
                 label = { Text("") }
             )
 
@@ -167,14 +159,9 @@ class MainActivity : ComponentActivity() {
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.End,
         ) {
-            Button(onClick = {
-                val sumNumPl = text.toString().toFloat() - text_1.toString().toFloat()
-                val str_sum = sumNumPl.toString()
-                text = str_sum
-            }) {
+            Button(onClick = {}) {
                 Text(text = "Отнять")
             }
-
         }
         Column(
             modifier = Modifier.padding(0.dp, 300.dp),
@@ -197,21 +184,25 @@ class MainActivity : ComponentActivity() {
         {
             TextField(
                 modifier = Modifier.width(70.dp),
-                value = text,
-                onValueChange = {
-                    text = it
-                },
+                value = "text",
+                onValueChange = {},
                 label = { Text("") }
             )
         }
+        Column(
+            modifier = Modifier.offset(260.dp, 280.dp),
+            ) {
+            Button(onClick = {}) {
+                Text(text = "Перейти")
+            }
+        }
     }
-
-
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
         LessonsTheme {
             Greeting("Android")
+
         }
     }
 }
---
+++ app/src/main/java/com/example/lessons/MainActivity2.kt
@@ -0,0 +1,93 @@
+package com.example.lessons
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lessons.ui.theme.LessonsTheme
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHost
+
+
+class MainActivity2 : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                val data = intent.getStringExtra("TextField")
+                Column(
+                    modifier = Modifier.padding(0.dp, 300.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.Start
+                )
+                {
+                    Row {
+                        Text(
+                            text = "Значение",
+                            modifier = Modifier.height(20.dp),
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier.padding(150.dp, 270.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        modifier = Modifier.width(70.dp),
+                        value = data.toString(),
+                        onValueChange = {},
+                        label = { Text(text = "") }
+                    )
+                }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.Start
+    )
+    {
+        Row {
+            Text(
+                text = "Значение",
+                modifier = Modifier.height(20.dp),
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LessonsTheme {
+        Greeting("Android")
+    }
+}
